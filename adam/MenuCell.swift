9ccd7e37a2d31c174d87775aacfc8e6135871a2b
TorIsHere <kittikorn.a@gmail.com>
2015-02-03 23:45:12 +0900

    Update MenuCell.swift
    remove debug code

--- adam/MenuCell.swift
+++ adam/MenuCell.swift
@@ -122,8 +122,8 @@ class MenuCell: UITableViewCell {
     }
     
     func setImageByURL(imgURL: NSURL) {
-        let url = NSURL(string: "http://greenasia-tokyo.com/wp/wp-content/uploads/2014/01/ga15.jpg")
-        let data = NSData(contentsOfURL: url!) //make sure your image in this url does exist, otherwise unwrap in a if let check
+        //let url = NSURL(string: "http://greenasia-tokyo.com/wp/wp-content/uploads/2014/01/ga15.jpg")
+        let data = NSData(contentsOfURL: imgURL) //make sure your image in this url does exist, otherwise unwrap in a if let check
         self.menuImageView.image = UIImage(data: data!)
         //imageURL.image = UIImage(data: data!)
         
