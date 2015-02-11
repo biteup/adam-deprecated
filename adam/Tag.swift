2a8d9828dab0ad4821778c046e58fa9f351c5933
Tor is Here <kittikorn.a@gmail.com>
2015-02-11 14:10:00 +0900

    Adding tags server API

---
+++ adam/Tag.swift
@@ -0,0 +1,36 @@
+//
+//  Tag.swift
+//  adam
+//
+//  Created by Kittikorn Ariyasuk on 2/10/15.
+//  Copyright (c) 2015 Benri. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class TagSVAPI {
+    
+    let apiBaseURL:String   = "http://snakebite.herokuapp.com"
+    let apiEndPoint:String  = "/tags"
+    
+    init(){
+        
+    }
+    func getTags(limit:Int, successCallback:(json:AnyObject?)->Void, errorCallback:()->Void){
+        
+        let my_response = Alamofire.request(.GET, apiBaseURL + apiEndPoint,parameters: ["limit": limit])
+            .responseJSON({ (req, res, json, error) in
+                if(error != nil) {
+                    NSLog("Error: \(error)")
+                    println(req)
+                    println(res)
+                    errorCallback()
+                }
+                else {
+                    successCallback(json: json)
+                }
+            })
+    }
+}
