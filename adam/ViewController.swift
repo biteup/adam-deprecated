3dc7a1ea31563c7f82d1fd395451fc91343bc08f
TorIsHere <kittikorn.a@gmail.com>
2015-03-24 18:22:40 +0900

    Quick Fix
    update JSON function
    update location

--- adam/ViewController.swift
+++ adam/ViewController.swift
@@ -60,6 +60,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDa
             self.locationManager.requestAlwaysAuthorization()
         } else {
             isInitiated = true
+            locationManager.startUpdatingLocation()
             self.populateMenu(true, tags: nil)
         }
     }
@@ -119,6 +120,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDa
                 NSUserDefaults.standardUserDefaults().setBool(true, forKey: "isLocationEnable")
             }
             if isUserAnswered {
+                locationManager.startUpdatingLocation()
                 self.initMenu()
                 if !self.isInitiated {
                     self.isInitiated = true
@@ -158,6 +160,56 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDa
         println("Error while updating location" + error.localizedDescription)
     }
     
+    func updateTableFromJSON(myJSON:JSON, isReset:Bool) {
+        
+        for (index: String, itemJSON: JSON) in myJSON["items"] {
+            if let storeName:String = itemJSON["name"].rawString() {
+                if let storeLocationStr = itemJSON["geolocation"].rawString()  {
+                    let longitudeDbl = itemJSON["geolocation"]["lon"].double!
+                    let latitudeDbl  = itemJSON["geolocation"]["lat"].double!
+                    let storeLocation = CLLocation(latitude: latitudeDbl, longitude: longitudeDbl)
+                    let storeDistance = self.locationService.getDistanceFrom(storeLocation)
+                    let storeAddress  = itemJSON["address"].string!
+                    
+                    for (index: String, menuJSON: JSON) in itemJSON["menus"] {
+                        var imgURLString:String = menuJSON["images"][0].string!
+                        
+                        imgURLString = imgURLString.stringByReplacingOccurrencesOfString("\"", withString: "", options:  NSStringCompareOptions.LiteralSearch, range: nil)
+                        
+                        if let menuName = menuJSON["name"].rawString() {
+                            if let imgURL = NSURL(string: imgURLString) {
+                                if let pointVal = menuJSON["rating"].int {
+                                    if let price    = menuJSON["price"].float {
+                                        var menu:Menu = Menu(menuName: menuName,
+                                            storeName: storeName,
+                                            imgURL: imgURL,
+                                            distanceVal: storeDistance,
+                                            pointVal: pointVal,
+                                            price: price,
+                                            address: storeAddress,
+                                            latitude: latitudeDbl,
+                                            longitude: longitudeDbl)
+                                        self.menuArray.addObject(menu)
+                                        
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if isReset {
+            dispatch_async(dispatch_get_main_queue()) {
+                self.menuTableView.reloadSections(NSIndexSet(index: 0), withRowAnimation: UITableViewRowAnimation.Bottom)
+            }
+        } else {
+            dispatch_async(dispatch_get_main_queue()) {
+                self.menuTableView.reloadData()
+            }
+        }
+    }
+    
     func populateMenu(isReset:Bool, tags: String?) -> Void {
         if self.isPopulating {
             return
@@ -177,56 +229,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDa
                     if let json: AnyObject = somejson{
                         self.currentLoadedIndex += self.populateLength
                         let myJSON = JSON(json)
-                        for (index: String, itemJSON: JSON) in myJSON["items"] {
-                            if let storeName:String = itemJSON["name"].rawString() {
-                                if let storeLocationStr = itemJSON["geolocation"].rawString()  {
-                                    let longitudeDbl = itemJSON["geolocation"]["lon"].double!
-                                    let latitudeDbl  = itemJSON["geolocation"]["lat"].double!
-                                    let storeLocation = CLLocation(latitude: latitudeDbl, longitude: longitudeDbl)
-                                    let storeDistance = self.locationService.getDistanceFrom(storeLocation)
-                                    let storeAddress  = itemJSON["address"].string!
-                                    
-                                    for (index: String, menuJSON: JSON) in itemJSON["menus"] {
-                                        var imgURLString:String = menuJSON["images"][0].string!
-                                        
-                                        imgURLString = imgURLString.stringByReplacingOccurrencesOfString("\"", withString: "", options:  NSStringCompareOptions.LiteralSearch, range: nil)
-                                        
-                                        if let menuName = menuJSON["name"].rawString() {
-                                            if let imgURL = NSURL(string: imgURLString) {
-                                                if let pointVal = menuJSON["rating"].int {
-                                                    if let price    = menuJSON["price"].float {
-                                                        var menu:Menu = Menu(menuName: menuName,
-                                                            storeName: storeName,
-                                                            imgURL: imgURL,
-                                                            distanceVal: storeDistance,
-                                                            pointVal: pointVal,
-                                                            price: price,
-                                                            address: storeAddress,
-                                                            latitude: latitudeDbl,
-                                                            longitude: longitudeDbl)
-                                                        self.menuArray.addObject(menu)
-                                                        
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                        if isReset {
-                            dispatch_async(dispatch_get_main_queue()) {
-                                self.menuTableView.reloadSections(NSIndexSet(index: 0), withRowAnimation: UITableViewRowAnimation.Bottom)
-                            }
-                        } else {
-                            dispatch_async(dispatch_get_main_queue()) {
-                                self.menuTableView.reloadData()
-                            }
-                            
-                            //self.menuTableView.reloadSections(NSIndexSet(index: 0), withRowAnimation: UITableViewRowAnimation.None)
-                        }
+                        self.updateTableFromJSON(myJSON, isReset: isReset)
                         self.isPopulating = false
-                        //activityView.stopAnimating()
                     }
                 },
                 {()->Void in
@@ -238,58 +242,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDa
                 {(somejson) -> Void in
                     if let json: AnyObject = somejson{
                         self.currentLoadedIndex += self.populateLength
-                        let lastItem = self.menuArray.count
-                        
                         let myJSON = JSON(json)
-                        for (index: String, itemJSON: JSON) in myJSON["items"] {
-                            if let storeName:String = itemJSON["name"].rawString() {
-                                if let storeLocationStr = itemJSON["geolocation"].rawString()  {
-                                    let longitudeDbl = itemJSON["geolocation"]["lon"].double!
-                                    let latitudeDbl  = itemJSON["geolocation"]["lat"].double!
-                                    let storeLocation = CLLocation(latitude: latitudeDbl, longitude: longitudeDbl)
-                                    let storeDistance = self.locationService.getDistanceFrom(storeLocation)
-                                    let storeAddress  = itemJSON["address"].string!
-                                    
-                                    for (index: String, menuJSON: JSON) in itemJSON["menus"] {
-                                        var imgURLString:String = menuJSON["images"][0].string!
-                                        
-                                        imgURLString = imgURLString.stringByReplacingOccurrencesOfString("\"", withString: "", options:  NSStringCompareOptions.LiteralSearch, range: nil)
-                                        
-                                        if let menuName = menuJSON["name"].rawString() {
-                                            if let imgURL = NSURL(string: imgURLString) {
-                                                if let pointVal = menuJSON["rating"].int {
-                                                    if let price    = menuJSON["price"].float {
-                                                        var menu = Menu(menuName: menuName,
-                                                            storeName: storeName,
-                                                            imgURL: imgURL,
-                                                            distanceVal: storeDistance,
-                                                            pointVal: pointVal,
-                                                            price: price,
-                                                            address: storeAddress,
-                                                            latitude: latitudeDbl,
-                                                            longitude: longitudeDbl)
-                                                        self.menuArray.addObject(menu)
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                        if isReset {
-                            dispatch_async(dispatch_get_main_queue()) {
-                                self.menuTableView.reloadSections(NSIndexSet(index: 0), withRowAnimation: UITableViewRowAnimation.Bottom)
-                            }
-                            
-                        } else {
-                            dispatch_async(dispatch_get_main_queue()) {
-                                self.menuTableView.reloadData()
-                                
-                                //self.menuTableView.
-                                //self.menuTableView.reloadSections(NSIndexSet(index: lastItem), withRowAnimation: UITableViewRowAnimation.Bottom)
-                            }
-                        }
+                        self.updateTableFromJSON(myJSON, isReset: isReset)
                         self.isPopulating = false
                     }
                 },
@@ -362,10 +316,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDa
             self.populateMenu(true, tags: searchTag)
         }
         const.deleteConst("search", key: "picker")
-    }
-    
-    func updateLocation() {
-        
     }
 }
 
